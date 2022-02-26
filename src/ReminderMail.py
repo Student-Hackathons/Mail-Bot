7b08ee264d2d9bd04b67208b612142fb9be49bbc
Sparsh Kesari <sparshkesari98@gmail.com>
2022-02-27 02:09:54 +0530

    template files added
    Signed-off-by: Sparsh Kesari <sparshkesari98@gmail.com>

---
+++ src/ReminderMail.py
@@ -0,0 +1,63 @@
+import smtplib
+import ssl
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+port = 465
+sender_mail = input("Enter the mail address of the hackathon organizer ")
+password = input("Input the password of above mail: ")
+no_hacker_mails = int(input("Input the no of hacker emails: "))
+hacker_mails = []
+
+for i in range(0, no_hacker_mails):
+    no = i
+    no += 1
+    up = repr(no)
+    no -= 1
+    statement = "Enter the mail no " + up + ": "
+    ele = input(statement)
+    hacker_mails.append(ele)
+
+
+message = MIMEMultipart("ABC Hackathon")
+message["Subject"] = "Reminder Mail"
+message["From"] = sender_mail
+message["To"] = ", ".join(hacker_mails)
+
+head = """\
+    ABC Hackathon
+    """
+html = """\
+<!DOCTYPE html>
+<html>
+<head>
+<title>Page Title</title>
+<style>
+body {
+  background-color: black;
+  text-align: center;
+  color: white;
+}
+</style>
+</head>
+<body>
+
+<h1>Reminder Heading</h1>
+<p> Insert Message here</p>
+
+</body>
+</html>
+"""
+
+div1 = MIMEText(head, "plain")
+div2 = MIMEText(html, "html")
+
+message.attach(div1)
+message.attach(div2)
+
+context = ssl.create_default_context()
+
+with smtplib.SMTP_SSL('smtp.gmail.com', port, context=context) as server:
+    server.login(sender_mail, password)
+    server.sendmail(sender_mail, hacker_mails, message.as_string())
+    print("Reminder Mail sent")
